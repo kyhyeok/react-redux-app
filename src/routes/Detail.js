a05685a8ec79890f873b2362e66087bfc2745993
kyhyeok <kyhyeok@gmail.com>
2020-09-14 10:36:41 +0900

    redux finished width bonus

---
+++ src/routes/Detail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <div>{toDo?.text}</div>
+      <h5>Created at :{toDo?.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
---
+++ src/routes/Home.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
+import { add } from "../store";
+
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+  function onChange(event) {
+    setText(event.target.value);
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={text}
+          placeholder="write"
+          onChange={onChange}
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(add(text))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
